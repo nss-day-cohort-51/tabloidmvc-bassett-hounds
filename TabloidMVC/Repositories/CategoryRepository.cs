fb3bdc88db291a8e5d8c9522888446d80b398d45
Matt F <trumpetman072392@gmail.com>
2022-01-04 10:01:45 -0600

    able to see list of categories

--- TabloidMVC/Repositories/CategoryRepository.cs
+++ TabloidMVC/Repositories/CategoryRepository.cs
@@ -14,7 +14,9 @@ namespace TabloidMVC.Repositories
                 conn.Open();
                 using (var cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = "SELECT id, name FROM Category";
+                    cmd.CommandText = @"SELECT id, name
+                                        FROM Category
+                                        Order by Name ASC ";
                     var reader = cmd.ExecuteReader();
 
                     var categories = new List<Category>();
